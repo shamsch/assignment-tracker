65081eabb5cba37bfd6b72932c61935ab5531129
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-14 17:18:37 +0200

    mode change toggle ready, logic yet to be made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,31 @@
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import Searchbar from './SearchBar.js'
-import { useMode } from '../hooks/useMode'
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Searchbar from "./SearchBar.js";
+import { useMode } from "../hooks/useMode";
+import modeIcon from "../asset/mode.svg";
 
 export default function NavBar() {
-    const {mode, changeMode} = useMode();
-    
-    return (
-        <div className='navbar'>
-            <nav>
-                <Link to={'/home'} className='brand'>
-                    <h1>Assignment Tracker</h1>
-                </Link>
-                <Searchbar />
-                <Link to={'/create'}>
-                    Create Assignment
-                </Link>
-            </nav>
-        </div>
-    )
+  const { mode, changeMode } = useMode();
+
+  const toggleMode = () => {
+    mode === "dark" ? changeMode("white") : changeMode("dark");
+    console.log(mode);
+  };
+
+  return (
+    <div>
+      <div className="navbar">
+        <nav>
+          <Link to={"/home"} className="brand">
+            <h1>Assignment Tracker</h1>
+          </Link>
+          <Searchbar />
+          <Link to={"/create"}>Create Assignment</Link>
+        </nav>
+      </div>
+      <div className="mode-div">
+        <img src={modeIcon} alt="Mode Icon" onClick={toggleMode}></img>
+      </div>
+    </div>
+  );
 }
