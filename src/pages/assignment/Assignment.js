3257dec92403607cc4b8fc898289230745a84f9f
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-16 18:07:44 +0200

    data loads when signle assignment opens

--- src/pages/assignment/Assignment.js
+++ src/pages/assignment/Assignment.js
@@ -1,23 +1,51 @@
-import {useParams} from 'react-router-dom'
-import {useFetch} from '../../hooks/useFetch'
-import { useMode } from '../../hooks/useMode';
-import './Assignment.css'
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useMode } from "../../hooks/useMode";
+import { project } from "../../firebase/config";
+import "./Assignment.css";
 
 export default function Assignment() {
-    const {id} = useParams();
-    const {data: assignment, isPending, error} = useFetch(`http://localhost:3000/assignments?id=${id}`)
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
 
-    const {mode}=useMode()
-    return (
-        <div className={`assignment ${mode}`}>
-            { error && <div className='error'>{error}</div> }
-            { isPending && <div>Loading...</div>}
-            { assignment
-             && <>
-                 <h1 className='page-title'>{assignment.at(0).title}</h1>
-                 <p> Time for the task {assignment.at(0).time}</p>
-                 <p>{assignment.at(0).details}</p>
-                </>}
-        </div>
-    )
+  useEffect(() => {
+    setIsPending(true);
+
+    const unsub = project.collection("assignments").onSnapshot(
+      (snapshot) => {
+        if (snapshot.empty) {
+          setError("No assignment to load");
+          setIsPending(false);
+        } else {
+          let result = {};
+          result = snapshot.docs.filter((doc) => doc.id === id);
+          setData(result.at(0).data());
+          setIsPending(false);
+        }
+      },
+      (err) => {
+        setError(err.message);
+        setIsPending(false);
+      }
+    );
+
+    return () => unsub();
+  }, [id]);
+
+  const { mode } = useMode();
+  return (
+    <div className={`assignment ${mode}`}>
+      {error && <div className="error">{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && (
+        <>
+          <h1 className="page-title">{data.title}</h1>
+          <p> Time for the task {data.time}</p>
+          <p>{data.details}</p>
+        </>
+      )}
+    </div>
+  );
 }
